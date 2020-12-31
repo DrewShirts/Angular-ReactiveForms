e9caf333742e4260ce1b460ce185ac3871f293b3
Drew Shirts <drewshirts@gmail.com>
2020-12-30 18:20:38 -0700

    Reactive Transformations

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators} from '@angular/forms';
+import {debounceTime} from 'rxjs/operators';
 
 import {Customer} from './customer';
 
@@ -62,9 +63,11 @@ export class CustomerComponent implements OnInit {
     );
 
     const emailControl = this.customerForm.get('emailGroup.email');
-    emailControl.valueChanges.subscribe(
+    emailControl.valueChanges.pipe(
+      debounceTime(1000)
+    ).subscribe(
       value => this.setMessage(emailControl)
-    )
+    );
   }
 
   populateTestData(): void {
